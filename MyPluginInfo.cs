b6c3ad253466d7f7a081c4a0b6d2455271e3ac8e
18107 <18107@users.noreply.github.com>
2023-12-18 09:26:08 +1030

    Add files via upload

---
+++ MyPluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace SprintInvert
+{
+    static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "id107.sprintinvert";
+        public const string PLUGIN_NAME = "SprintInvert";
+        public const string PLUGIN_VERSION = "0.0.0";
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Sprint Invert
+For Game Version: 0.24.0  
+Mod Version: 0.0.0  
+Developed by: 18107  
+Host/Client Requirements: Client
+
+Sprint by default. Hold the sprint key to walk.
