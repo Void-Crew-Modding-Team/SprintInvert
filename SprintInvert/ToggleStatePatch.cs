196a737a1db83abab706fb059cf8f758ee062f9b
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-03-30 19:04:53 -0700

    Simplified the patches

---
+++ SprintInvert/ToggleStatePatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Opsive.UltimateCharacterController.Character.Abilities;
+
+namespace SprintInvert
+{
+    [HarmonyPatch(typeof(SpeedChangeToggle), "ChangeActiveState")]
+    class ToggleStatePatch
+    {
+        static void Prefix(ref bool active)
+        {
+            active = !active;
+        }
+    }
+}
