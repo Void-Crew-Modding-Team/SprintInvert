fad4bc29e73ee8b62b34b35b845d412bb00b80f6
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-03-31 20:33:59 -0700

    Added GUI

---
+++ SprintInvert/GUI.cs
@@ -0,0 +1,25 @@
+﻿using VoidManager.CustomGUI;
+using static UnityEngine.GUILayout;
+
+namespace SprintInvert
+{
+    internal class GUI : ModSettingsMenu
+    {
+        public override string Name()
+        {
+            return "Sprint Inverted: " + BepinPlugin.SprintInvertEnabled.Value;
+        }
+
+        public override void Draw()
+        {
+            if (Button("Sprint Inverted: " + BepinPlugin.SprintInvertEnabled.Value))
+            {
+                BepinPlugin.SprintInvertEnabled.Value = !BepinPlugin.SprintInvertEnabled.Value;
+            }
+            if (Button("Tap Toggle: " + (BepinPlugin.TapToggleEnabled.Value ? "Enabled" : "Disabled")))
+            {
+                BepinPlugin.TapToggleEnabled.Value = !BepinPlugin.TapToggleEnabled.Value;
+            }
+        }
+    }
+}
